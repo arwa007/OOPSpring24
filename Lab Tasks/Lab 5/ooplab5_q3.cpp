4b05e65741e5fa0dc0d22ea1119ec25f76da6c44
arwa007 <142319755+arwa007@users.noreply.github.com>
2024-03-07 16:53:39 +0500

    Add files via upload

---
+++ Lab Tasks/Lab 5/ooplab5_q3.cpp
@@ -0,0 +1,41 @@
+/*
+Name: Arwa Feroze
+ID: 23k-0022
+Lab 5 Task 3
+*/
+#include <iostream>
+#include <string>
+#include <cctype>
+using namespace std;
+
+class validateString{
+
+string input;
+
+public:
+
+validateString(string input){
+    this->input = input;
+}
+bool isValid() const{
+
+    for (char c : input){
+        if (!isalpha(c)){
+            return false;
+        }
+    }
+
+    return true;
+}
+};
+
+int main() {
+
+    validateString str1("HelloWorld");
+    validateString str2("abc123");
+
+    cout << "String 1 is " << (str1.isValid() ? "valid" : "invalid") << endl;
+    cout << "String 2 is " << (str2.isValid() ? "valid" : "invalid") << endl;
+
+    return 0;
+}
